bdf1494f45df2c1a7f16bb2c9986bebe7f5d2eed
Makoto-Yuki1 <dragonoinvencivel@gmail.com>
2025-09-30 11:41:26 -0300

    Add files via upload

---
+++ continuação de java.script (dia 5 ou 6 eu acho)/script2.js
@@ -0,0 +1,61 @@
+//arrays -> vetores, matrizes
+// estrutura de dados para armazenar um conjunto de elementos
+let frutas = ["banana", "maça", "melancia"]; //array de tamanho 3
+console.log(frutas);
+console.log(frutas[1]);
+//a contagem se inicia do 0, nesse caso o 0 = banana
+let numeros = new Array(2,8,9,10);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "gaby";
+console.log(arrayVazio);
+
+//tamanho do Array -> length
+let dados = [10,50,30]; //tamanho 5 -> length 5
+console.log(dados.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
