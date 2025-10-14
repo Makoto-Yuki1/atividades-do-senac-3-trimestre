cef7c811e81e19d8378adfbc8858c97c01194caa
Makoto-Yuki1 <dragonoinvencivel@gmail.com>
2025-10-14 12:15:25 -0300

    Add files via upload

---
+++ treinando array/atividade.js
@@ -0,0 +1,14 @@
+let inscritos = [];
+inscritos.push("João Silva", "Maria Santos", "Pedro Almeida");
+console.log(inscritos);
+
+inscritos.length = 3;
+
+
+console.log("Antes da adição:", inscritos);
+inscritos.push("Ana Pereira");
+console.log("Depois da adição:", inscritos + "Ana Pereira");
+
+onsole.log("Antes da adição:", inscritos);
+inscritos.unshift("VIP Carlos Mendes" );
+console.log("Depois da adição:", inscritos + "Ana Pereira");
